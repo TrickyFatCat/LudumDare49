f80fab19b1eed00d0f3231c5ee7abd2129c21abd
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-02 17:01:26 +0300

    Implement explosive barrel

---
+++ Source/LudumDare49/Private/Actors/ExplosiveBarrel.cpp
@@ -0,0 +1,65 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+
+#include "Actors/ExplosiveBarrel.h"
+#include "Weapons/WeaponFxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+AExplosiveBarrel::AExplosiveBarrel()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	BarrelMesh = CreateDefaultSubobject<UStaticMeshComponent>("BarrelMesh");
+	SetRootComponent(BarrelMesh);
+
+	ExplosionFXComponent = CreateDefaultSubobject<UWeaponFxComponent>("ExplosionFXComponent");
+}
+
+void AExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AExplosiveBarrel::OnTakeDamage(AActor* DamagedActor,
+	float Damage,
+	const UDamageType* DamageType,
+	AController* InstigatedBy,
+	AActor* DamageCauser)
+{
+	if (bIsExploded) return;
+
+	ExplosionDelay = ExplosionDelay <= 0.f ? GetWorld()->GetTimeSeconds() : ExplosionDelay;
+	GetWorldTimerManager().SetTimer(ExplosionDelayHandle,
+	                                this,
+	                                &AExplosiveBarrel::ProcessExplosion,
+	                                ExplosionDelay,
+	                                false);
+	
+	bIsExploded = true;
+}
+
+void AExplosiveBarrel::ProcessExplosion()
+{
+	UGameplayStatics::ApplyRadialDamage(GetWorld(),
+	                                    ExplosionDamage,
+	                                    GetActorLocation(),
+	                                    ExplosionRadius,
+	                                    DamageTypeClass,
+	                                    {},
+	                                    this,
+	                                    nullptr,
+	                                    bDoFullDamage,
+	                                    ECC_GameTraceChannel1);
+
+	FHitResult HitResult;
+	HitResult.ImpactPoint = GetActorLocation();
+	HitResult.ImpactNormal = GetActorUpVector();
+	ExplosionFXComponent->PlayImpactFX(HitResult);
+	Destroy();
+}
+
