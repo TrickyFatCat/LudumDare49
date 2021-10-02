7e88375c10cf2420df6be0fedfda2a25fed6a103
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-02 09:02:43 +0300

    Create weapon component class

---
+++ Source/LudumDare49/Private/Components/WeaponComponent.cpp
@@ -0,0 +1,262 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+
+#include "Components/WeaponComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Characters/PlayerCharacter.h"
+
+UWeaponComponent::UWeaponComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	EquipAnimationTimeline = CreateDefaultSubobject<UTimelineComponent>("PullAnimationTimeline");
+}
+
+
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Weapons[0].bIsAvailable = true;
+	EquipWeapon(CurrentWeaponIndex);
+
+	EquipAnimationTimeline->SetPlayRate(1.f / (EquipDuration * 0.5f));
+
+	if (EquipAnimationCurve)
+	{
+		FOnTimelineFloat AnimationProgress;
+		AnimationProgress.BindUFunction(this, FName("SetPullProgress"));
+		EquipAnimationTimeline->AddInterpFloat(EquipAnimationCurve, AnimationProgress);
+
+		FOnTimelineEvent AnimationFinished;
+		AnimationFinished.BindUFunction(this, FName("OnPullFinished"));
+		EquipAnimationTimeline->SetTimelineFinishedFunc(AnimationFinished);
+	}
+}
+
+
+void UWeaponComponent::TickComponent(float DeltaTime,
+                                     ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UWeaponComponent::SpawnWeapons(USceneComponent* WeaponScene)
+{
+	if (!GetWorld() || !GetOwner()) return;
+
+	APlayerCharacter* PlayerCharacter = Cast<APlayerCharacter>(GetOwner());
+
+	if (!PlayerCharacter) return;
+
+	for (auto WeaponClass : WeaponClasses)
+	{
+		AWeaponBase* Weapon = GetWorld()->SpawnActor<AWeaponBase>(WeaponClass);
+
+		if (!Weapon) continue;
+
+		Weapon->SetOwner(GetOwner());
+		Weapons.Add(FWeaponInventoryData{Weapon, false});
+		FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::SnapToTarget, false);
+		Weapon->AttachToComponent(WeaponScene, AttachmentTransformRules);
+		Weapon->SetActorHiddenInGame(true);
+		Weapon->OnMakeShot.AddUObject(this, &UWeaponComponent::OnWeaponMakeShot);
+	}
+}
+
+void UWeaponComponent::GetCurrentWeaponData(FWeaponData& WeaponData) const
+{
+	CurrentWeapon->GetWeaponData(WeaponData);
+}
+
+void UWeaponComponent::EquipNextWeapon()
+{
+	if (bIsEquipping || bIsReloading) return;
+
+	PreviousWeaponIndex = CurrentWeaponIndex;
+
+	do
+	{
+		CurrentWeaponIndex = (CurrentWeaponIndex + 1) % Weapons.Num();
+	}
+	while (!Weapons[CurrentWeaponIndex].bIsAvailable);
+
+	if (PreviousWeaponIndex == CurrentWeaponIndex) return;
+
+	CurrentWeapon->StopShooting();
+	StartEquipAnimation();
+}
+
+void UWeaponComponent::EquipPreviousWeapon()
+{
+	if (bIsEquipping || bIsReloading) return;
+
+	PreviousWeaponIndex = CurrentWeaponIndex;
+
+	do
+	{
+		CurrentWeaponIndex = CurrentWeaponIndex == 0 ? Weapons.Num() - 1 : CurrentWeaponIndex - 1;
+	}
+	while (!Weapons[CurrentWeaponIndex].bIsAvailable);
+
+	if (PreviousWeaponIndex == CurrentWeaponIndex) return;
+
+	CurrentWeapon->StopShooting();
+	StartEquipAnimation();
+}
+
+void UWeaponComponent::StartShooting()
+{
+	if (!CanShoot()) return;
+
+	CurrentWeapon->StartShooting();
+	bIsShooting = true;
+}
+
+void UWeaponComponent::StopShooting()
+{
+	if (!CurrentWeapon) return;
+
+	CurrentWeapon->StopShooting();
+	bIsShooting = false;
+}
+
+bool UWeaponComponent::UnlockWeapon(TSubclassOf<AWeaponBase> WeaponClass)
+{
+	bool Result = false;
+
+	for (int32 i = 0; i < Weapons.Num(); ++i)
+	{
+		FWeaponInventoryData& InventoryData = Weapons[i];
+		if (!InventoryData.Weapon->IsA(WeaponClass)) continue;
+
+		if (InventoryData.bIsAvailable)
+		{
+			Result = false;
+			break;
+		};
+
+		InventoryData.bIsAvailable = true;
+
+		// if (!bIsReloading && !bIsEquipping && !RecoilTimeline->IsPlaying())
+		if (!bIsReloading && !bIsEquipping)
+		{
+			PreviousWeaponIndex = CurrentWeaponIndex;
+			CurrentWeaponIndex = i;
+			StartEquipAnimation();
+		}
+
+		Result = true;
+		break;
+	}
+
+	return Result;
+}
+
+bool UWeaponComponent::RestoreAmmo(TSubclassOf<AWeaponBase> WeaponClass, const int32 Amount)
+{
+	bool Result = false;
+
+	if (Amount <= 0) return Result;
+
+	for (int32 i = 0; i < Weapons.Num(); ++i)
+	{
+		const FWeaponInventoryData& InventoryData = Weapons[i];
+
+		if (!InventoryData.Weapon->IsA(WeaponClass)) continue;
+
+		if (InventoryData.Weapon->AmmoIsFull())
+		{
+			Result = false;
+			break;
+		}
+
+		InventoryData.Weapon->IncreaseAmmo(Amount);
+		Result = true;
+		break;
+	}
+
+	return Result;
+}
+
+void UWeaponComponent::EquipWeapon(const int32 WeaponIndex)
+{
+	if (!GetOwner() || WeaponIndex >= Weapons.Num() || Weapons.Num() == 0) return;
+
+	if (CurrentWeapon)
+	{
+		CurrentWeapon->StopShooting();
+		CurrentWeapon->SetActorHiddenInGame(true);
+	}
+
+	CurrentWeapon = Weapons[WeaponIndex].Weapon;
+	CurrentWeapon->SetActorHiddenInGame(false);
+	// RecoilTimeline->SetPlayRate(1.f / (FMath::Min(RecoilDuration, CurrentWeapon->GetTimeBetweenShots()) * 0.5f));
+
+	FWeaponData WeaponData;
+	CurrentWeapon->GetWeaponData(WeaponData);
+}
+
+void UWeaponComponent::OnWeaponMakeShot()
+{
+	OnWeaponShot.Broadcast();
+}
+
+void UWeaponComponent::AnimateWeaponEquip(const float Value)
+{
+	EquipProgress = Value;
+
+	FVector NewLocation = FVector::ZeroVector;
+	FRotator NewRotation = FRotator::ZeroRotator;
+
+	FVector TargetLocation = NewLocation;
+	TargetLocation.Z = NewLocation.Z - LocationOffset;
+	FRotator TargetRotation = NewRotation;
+	TargetRotation.Roll = NewRotation.Roll - RotationOffset;
+
+	NewLocation = NewLocation + TargetLocation * Value;
+	NewRotation = NewRotation + TargetRotation * Value;
+	
+	CurrentWeapon->SetActorRelativeLocation(NewLocation);
+	CurrentWeapon->SetActorRelativeRotation(NewRotation);
+}
+
+void UWeaponComponent::OnEquipAnimationFinished()
+{
+	FWeaponData WeaponData;
+	CurrentWeapon->GetWeaponData(WeaponData);
+
+	if (EquipProgress >= 1.f)
+	{
+		EquipWeapon(CurrentWeaponIndex);
+		EquipAnimationTimeline->ReverseFromEnd();
+		return;
+	}
+
+	bIsEquipping = false;
+
+	if (bIsShooting)
+	{
+		StartShooting();
+	}
+}
+
+void UWeaponComponent::StartEquipAnimation()
+{
+	bIsEquipping = true;
+
+	if (EquipDuration <= 0.f)
+	{
+		EquipWeapon(CurrentWeaponIndex);
+		bIsEquipping = false;
+		return;
+	}
+
+	EquipAnimationTimeline->PlayFromStart();
+}
+
+void UWeaponComponent::GetCurrentWeaponAmmo(FWeaponAmmoData& AmmoData) const
+{
+	AmmoData = CurrentWeapon->GetAmmoData();
+}
---
+++ Source/LudumDare49/Public/Components/WeaponComponent.h
@@ -0,0 +1,144 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Weapons/WeaponCoreTypes.h"
+#include "Weapons/WeaponBase.h"
+#include "WeaponComponent.generated.h"
+
+class UTimelineComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUDUMDARE49_API UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWeaponComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime,
+	                           ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	void SpawnWeapons(USceneComponent* WeaponScene);
+
+	UFUNCTION(BlueprintGetter, Category="Weapon")
+	AWeaponBase* GetCurrentWeapon() const { return CurrentWeapon; }
+
+	UFUNCTION(BlueprintPure, Category="Weapon")
+	void GetCurrentWeaponData(FWeaponData& WeaponData) const;
+
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Weapon", meta=(AllowPrivateAccess="true"))
+	TArray<TSubclassOf<AWeaponBase>> WeaponClasses;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Weapon", meta=(AllowPrivateAccess="true"))
+	TArray<FWeaponInventoryData> Weapons;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurrentWeapon)
+	AWeaponBase* CurrentWeapon = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly)
+	int32 CurrentWeaponIndex = 0;
+
+	UPROPERTY(VisibleInstanceOnly)
+	int32 PreviousWeaponIndex = 0;
+
+	bool bWeaponsSpawned = false;
+
+	// Weapon control
+public:
+	UPROPERTY(BlueprintAssignable, Category="Weapon")
+	FOnWeaponShotSignature OnWeaponShot;
+
+	void EquipNextWeapon();
+
+	void EquipPreviousWeapon();
+
+	void StartShooting();
+
+	void StopShooting();
+
+	bool UnlockWeapon(TSubclassOf<AWeaponBase> WeaponClass);
+
+	bool RestoreAmmo(TSubclassOf<AWeaponBase> WeaponClass, const int32 Amount);
+
+	UFUNCTION(BlueprintPure, Category="Weapon")
+	bool GetIsShooting() const { return CanShoot() && bIsShooting; }
+
+protected:
+	bool bIsShooting;
+
+	FTimerHandle ReloadTimerHandle;
+
+	void EquipWeapon(const int32 WeaponIndex);
+
+private:
+	const float WallCheckDistance = 80.f;
+
+	void OnWeaponMakeShot();
+
+	// Equip animation
+public:
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation|Equip", meta=(AllowPrivateAccess="true"))
+	UCurveFloat* EquipAnimationCurve = nullptr;
+
+	UPROPERTY()
+	UTimelineComponent* EquipAnimationTimeline = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Animation|Equip", meta=(AllowPrivateAccess="true"))
+	float EquipProgress = 0.f;
+
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadOnly,
+		Category="Animation|Equip",
+		meta=(AllowPrivateAccess="true", ClampMin="0"))
+	float EquipDuration = 0.25f;
+
+	UPROPERTY(EditDefaultsOnly, Category="Animation|Equip")
+	float RotationOffset = 70;
+
+	UPROPERTY(EditDefaultsOnly, Category="Animation|Equip")
+	float LocationOffset = 70;
+
+	UFUNCTION()
+	void AnimateWeaponEquip(const float Value);
+
+	UFUNCTION()
+	void OnEquipAnimationFinished();
+
+	void StartEquipAnimation();
+
+	// Weapon parameters
+public:
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	bool CanShoot() const
+	{
+		return CurrentWeapon->CanShoot() && !bIsReloading && !bIsEquipping;
+	}
+
+	UFUNCTION(BlueprintGetter, Category="Weapon")
+	bool GetIsReloading() const { return bIsReloading; }
+
+	UFUNCTION(BlueprintGetter, Category="Weapon")
+	bool GetIsEquipping() const { return bIsEquipping; }
+
+	UFUNCTION(BlueprintCallable, Category="Weapon")
+	void GetCurrentWeaponAmmo(FWeaponAmmoData& AmmoData) const;
+
+protected:
+private:
+	UPROPERTY(BlueprintGetter=GetIsReloading, Category="Weapon")
+	bool bIsReloading = false;
+
+	UPROPERTY(BlueprintGetter=GetIsEquipping, Category="Weapon")
+	bool bIsEquipping = false;
+};
