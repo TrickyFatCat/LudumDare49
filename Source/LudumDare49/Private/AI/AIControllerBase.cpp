e7379202a8bb89a223b462ed54d715b2776c53ab
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-03 08:27:33 +0300

    Update AI controller

--- Source/LudumDare49/Private/AI/AIControllerBase.cpp
+++ Source/LudumDare49/Private/AI/AIControllerBase.cpp
@@ -2,4 +2,67 @@
 
 
 #include "AI/AIControllerBase.h"
+#include "AI/AIPerceptionBase.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Characters/EnemyCharacterBase.h"
 
+AAIControllerBase::AAIControllerBase()
+{
+	Perception = CreateDefaultSubobject<UAIPerceptionBase>("PerceptionComponent");
+}
+
+void AAIControllerBase::BeginPlay()
+{
+	Super::BeginPlay();
+	UBlackboardComponent* BlackboardComponent = GetBlackboardComponent();
+
+	if (BlackboardComponent)
+	{
+		BlackboardComponent->SetValueAsVector(InitialLocationKeyName, GetPawn()->GetActorLocation());
+
+		AEnemyCharacterBase* EnemyCharacter = Cast<AEnemyCharacterBase>(GetPawn());
+	}
+}
+
+void AAIControllerBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AAIControllerBase::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	AEnemyCharacterBase* AICharacter = Cast<AEnemyCharacterBase>(InPawn);
+
+	if (AICharacter)
+	{
+		RunBehaviorTree(AICharacter->GetBehaviorTree());
+	}
+
+	if (GetBlackboardComponent())
+	{
+		GetBlackboardComponent()->SetValueAsBool(IsAttackingKeyName, false);
+	}
+}
+
+AActor* AAIControllerBase::GetTargetActor() const
+{
+	if (!GetBlackboardComponent()) return nullptr;
+
+	return Cast<AActor>(GetBlackboardComponent()->GetValueAsObject(TargetActorKeyName));
+}
+
+void AAIControllerBase::SetTargetActor(AActor* Value)
+{
+	if (!GetBlackboardComponent()) return;
+
+	GetBlackboardComponent()->SetValueAsObject(TargetActorKeyName, Value);	
+}
+
+void AAIControllerBase::SetIsAttacking(bool Value)
+{
+	if (!GetBlackboardComponent()) return;
+
+	GetBlackboardComponent()->SetValueAsBool(IsAttackingKeyName, Value);
+}
--- Source/LudumDare49/Public/AI/AIControllerBase.h
+++ Source/LudumDare49/Public/AI/AIControllerBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "AI/AIPerceptionBase.h"
 #include "AIControllerBase.generated.h"
 
 /**
@@ -13,5 +14,38 @@ UCLASS()
 class LUDUMDARE49_API AAIControllerBase : public AAIController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAIControllerBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+
+	// Behaviour tree
+public:
+	AActor* GetTargetActor() const;
+
+	void SetTargetActor(AActor* Value);
+
+	void SetIsAttacking(bool Value);
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="BehaviorTree", meta=(AllowPrivateAccess="true"))
+	FName TargetActorKeyName = "TargetActor";
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="BehaviorTree", meta=(AllowPrivateAccess="true"))
+	FName InitialLocationKeyName = "InitialLocation";
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="BehaviorTree", meta=(AllowPrivateAccess="true"))
+	FName IsAttackingKeyName = "IsAttacking";
+
+	// Perception
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
+	UAIPerceptionBase* Perception = nullptr;
 };
