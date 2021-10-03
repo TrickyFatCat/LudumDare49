df7aa89bbca7d9b41c29fff7107f7aeddc527f4d
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-03 18:07:06 +0300

    Create melee enemy class

---
+++ Source/LudumDare49/Private/Characters/EnemyMelee.cpp
@@ -0,0 +1,5 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+
+#include "Characters/EnemyMelee.h"
+
---
+++ Source/LudumDare49/Public/Characters/EnemyMelee.h
@@ -0,0 +1,17 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/EnemyCharacterBase.h"
+#include "EnemyMelee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE49_API AEnemyMelee : public AEnemyCharacterBase
+{
+	GENERATED_BODY()
+	
+};
