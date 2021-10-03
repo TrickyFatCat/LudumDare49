0311acf3b4768197b03d1700563f8285df9ed0ff
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-03 07:35:55 +0300

    Create base enemy AI controller

---
+++ Source/LudumDare49/Private/AI/AIControllerBase.cpp
@@ -0,0 +1,5 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+
+#include "AI/AIControllerBase.h"
+
---
+++ Source/LudumDare49/Public/AI/AIControllerBase.h
@@ -0,0 +1,17 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE49_API AAIControllerBase : public AAIController
+{
+	GENERATED_BODY()
+	
+};
