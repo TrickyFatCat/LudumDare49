2d611e33bc1505b7265c95636267f8b419bbbb42
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-03 07:48:08 +0300

    Implement damage report

--- Source/LudumDare49/Private/Characters/EnemyCharacterBase.cpp
+++ Source/LudumDare49/Private/Characters/EnemyCharacterBase.cpp
@@ -3,3 +3,51 @@
 
 #include "Characters/EnemyCharacterBase.h"
 
+#include "Components/DamageControllerComponent.h"
+#include "Perception/AISense_Damage.h"
+
+AEnemyCharacterBase::AEnemyCharacterBase()
+{
+}
+
+void AEnemyCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	OnTakeAnyDamage.AddDynamic(this, &AEnemyCharacterBase::OnAnyDamage);
+}
+
+void AEnemyCharacterBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AEnemyCharacterBase::AttackPlayer()
+{
+}
+
+void AEnemyCharacterBase::OnAnyDamage(AActor* DamageActor,
+                                      float Damage,
+                                      const UDamageType* DamageType,
+                                      AController* InstigatedBy,
+                                      AActor* DamageCauser)
+{
+	if (DamageController->GetIsDead()) return;
+
+	ReportDamageEvent(Damage, InstigatedBy, DamageCauser);
+}
+
+void AEnemyCharacterBase::ReportDamageEvent(const float Damage,
+                                            const AController* InstigatedBy,
+                                            const AActor* Causer) const
+{
+	AActor* InstigatorPawn = InstigatedBy == nullptr ? nullptr : InstigatedBy->GetPawn();
+	const FVector EventLocation = InstigatedBy == nullptr
+		                              ? Causer->GetActorLocation()
+		                              : InstigatorPawn->GetActorLocation();
+	UAISense_Damage::ReportDamageEvent(GetWorld(),
+	                                   GetOwner(),
+	                                   InstigatorPawn,
+	                                   Damage,
+	                                   EventLocation,
+	                                   GetOwner()->GetActorLocation());
+}
--- Source/LudumDare49/Public/Characters/EnemyCharacterBase.h
+++ Source/LudumDare49/Public/Characters/EnemyCharacterBase.h
@@ -13,5 +13,27 @@ UCLASS()
 class LUDUMDARE49_API AEnemyCharacterBase : public ACharacterBase
 {
 	GENERATED_BODY()
+
+public:
+	AEnemyCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+	virtual void AttackPlayer();
+
+private:
+	UFUNCTION()
+	void OnAnyDamage(AActor* DamageActor,
+	                 float Damage,
+	                 const UDamageType* DamageType,
+	                 AController* InstigatedBy,
+	                 AActor* DamageCauser);
+
 	
+	void ReportDamageEvent(const float Damage, const AController* InstigatedBy, const AActor* Causer) const;
 };
