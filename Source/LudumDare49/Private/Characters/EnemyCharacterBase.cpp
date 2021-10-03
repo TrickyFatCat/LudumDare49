9744ba39b5430346fe798eda8e8d99a6891b1c10
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-03 07:35:31 +0300

    Create base enemy character class

---
+++ Source/LudumDare49/Private/Characters/EnemyCharacterBase.cpp
@@ -0,0 +1,5 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+
+#include "Characters/EnemyCharacterBase.h"
+
---
+++ Source/LudumDare49/Public/Characters/EnemyCharacterBase.h
@@ -0,0 +1,17 @@
+// Made by Title Goose Team during LudumDare 49. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CharacterBase.h"
+#include "EnemyCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE49_API AEnemyCharacterBase : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
